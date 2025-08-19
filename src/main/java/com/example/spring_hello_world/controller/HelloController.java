068905573ce72e27dd930939aafc9d3643bb62ba
Ahmed-Amer01 <ahmedamer42214953@gmail.com>
2025-08-19 16:37:41 +0300

    Add Controller Folder

--- src/main/java/com/example/spring_hello_world/controller/HelloController.java
+++ src/main/java/com/example/spring_hello_world/controller/HelloController.java
@@ -1,13 +1,12 @@
-package com.example.spring_hello_world;
+package com.example.spring_hello_world.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/hello")
+
+    @GetMapping("/api/hello")
     public String hello() {
         return "Hello World";
     }
