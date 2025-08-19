7c74bd8cef8f64f956c74f258ff86ec5b06a26f4
Ahmed-Amer01 <ahmedamer42214953@gmail.com>
2025-08-19 16:25:50 +0300

    first commit

---
+++ src/main/java/com/example/spring_hello_world/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.spring_hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
